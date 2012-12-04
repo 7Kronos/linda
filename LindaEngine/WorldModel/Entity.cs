70b1514f57129e06071d11290fa160b86cd3676e
Tarik TIRE <kronos@aspbooster.com>
2012-12-04 19:50:58 +0100

    Initial setup

---
+++ LindaEngine/WorldModel/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Persistence.WorldModel
+{
+	/// <summary>
+	/// Un élément qui peut être persisté
+	/// </summary>
+	public abstract class Entity
+	{
+		public Guid ID { get; internal set; }
+	}
+}
---
+++ LindaEngine/WorldModel/Persistence/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Persistence.WorldModel;
+using SquareArch.DomainModel;
+
+namespace SquareArch.DomainModel.Persistence
+{
+	public interface IRepository<T> where T : Entity
+	{
+		T Get(Guid ID);
+		IEnumerable<T> Get();
+		IEnumerable<T> Get(Func<T, bool> filter);
+
+		void Add(T item);
+		void Delete(T item);
+		void Update(T item);
+	}
+}
